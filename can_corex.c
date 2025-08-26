bb241aa480e3b2e88507e38c95a0a1d863b4ec7a
unknown <adrianp126p@gmail.com>
2025-08-26 21:41:22 +0200

    init

---
+++ can_corex.c
@@ -0,0 +1,246 @@
+/*
+ * can_corex.c
+ *
+ *  Created on: Aug 20, 2025
+ *      Author: Adrian
+ */
+
+#include "can_corex.h"
+#include "assert.h"
+#include <stddef.h>
+
+#if CCX_TICK_FROM_FUNC
+
+CCX_TIME_t (*CCX_get_tick)(void) = NULL;
+
+#define CCX_GET_TICK ((CCX_get_tick != NULL) ? CCX_get_tick() : ((CCX_TIME_t)0))
+
+void CCX_tick_function_register(CCX_TIME_t (*Function)(void))
+{
+    assert(Function != NULL);
+
+    CCX_get_tick = Function;
+}
+
+#else
+
+CCX_TIME_t *CCX_tick = NULL;
+
+#define CCX_GET_TICK (*(CCX_tick))
+
+void CCX_tick_variable_register(CCX_TIME_t *Variable)
+{
+    assert(Variable != NULL);
+
+    CCX_tick = Variable;
+}
+
+#endif
+
+
+static inline void CopyBuf(uint8_t *restrict src, uint8_t *restrict dst, size_t size)
+{
+    assert((src != NULL) && (dst != NULL));
+
+    for (size_t i = 0; i < size; i++)
+    {
+        dst[i] = src[i];
+    }
+}
+
+
+
+
+
+
+void CCX_RX_PushMsg(CCX_instance_t *Instance, uint32_t ID, uint8_t *Data, uint8_t DLC, uint8_t IDE_flag)
+{
+    assert(Instance != NULL);
+
+    uint16_t next_head = Instance->RxHead + 1;
+    if (next_head >= CCX_RX_BUFFER_SIZE)
+    {
+        next_head = 0;
+    }
+
+    if (next_head == Instance->RxTail)
+    {
+        return;
+    }
+
+    Instance->RxHead = next_head;
+
+    Instance->RxBuf[Instance->RxHead].ID = ID;
+    Instance->RxBuf[Instance->RxHead].DLC = DLC;
+    Instance->RxBuf[Instance->RxHead].IDE_flag = IDE_flag;
+    Instance->RxReceivedTick[Instance->RxHead] = CCX_GET_TICK;
+
+    if ((DLC > 0) && (DLC <= 8))
+    {
+        CopyBuf(Data, Instance->RxBuf[Instance->RxHead].Data, DLC);
+    }
+}
+
+static inline CCX_MsgRegStatus_t CCX_RX_MsgFromTables(CCX_instance_t *Instance, CCX_message_t *Msg)
+{
+    assert(Instance != NULL);
+
+    if (NULL == Instance->CCX_RX_table)
+    {
+        return CCX_MSG_UNREG;
+    }
+
+    for (uint16_t i = 0; i < Instance->RxTableSize; i++)
+    {
+        if (Instance->CCX_RX_table[i].ID == Msg->ID)
+        {
+            if ((Instance->CCX_RX_table[i].DLC == Msg->DLC) && (Instance->CCX_RX_table[i].IDE_flag == Msg->IDE_flag))
+            {
+                Instance->CCX_RX_table[i].Parser(Instance, Msg, i);
+                Instance->CCX_RX_table[i].LastTick = Instance->RxReceivedTick[Instance->RxTail];
+                return CCX_MSG_REG;
+            }
+        }
+    }
+    return CCX_MSG_UNREG;
+}
+
+static inline void CCX_Timeout_Check(CCX_instance_t *Instance)
+{
+    if (NULL != Instance->CCX_RX_table)
+    {
+        for (uint16_t i = 0; i < Instance->RxTableSize; i++)
+        {
+            if ((0 != Instance->CCX_RX_table[i].TimeOut) &&
+                (CCX_GET_TICK - Instance->CCX_RX_table[i].LastTick >= Instance->CCX_RX_table[i].TimeOut))
+            {
+                Instance->CCX_RX_table[i].LastTick = CCX_GET_TICK;
+                if (NULL != Instance->TimeoutCallback)
+                {
+                    Instance->TimeoutCallback(Instance, i);
+                }
+            }
+        }
+    }
+}
+
+static inline void CCX_RX_Poll(CCX_instance_t *Instance)
+{
+    assert(Instance != NULL);
+
+    while (Instance->RxHead != Instance->RxTail)
+    {
+        Instance->RxTail++;
+        if (Instance->RxTail >= CCX_RX_BUFFER_SIZE)
+        {
+            Instance->RxTail = 0;
+        }
+
+        if ((CCX_RX_MsgFromTables(Instance, &Instance->RxBuf[Instance->RxTail]) != CCX_MSG_REG) &&
+            NULL != Instance->Parser_unreg_msg)
+        {
+            Instance->Parser_unreg_msg(Instance, &Instance->RxBuf[Instance->RxTail]);
+        }
+    }
+
+    CCX_Timeout_Check(Instance);
+}
+
+
+void CCX_TX_PushMsg(CCX_instance_t *Instance, uint32_t ID, uint8_t *Data, uint8_t DLC, uint8_t IDE_flag)
+{
+    assert(Instance != NULL);
+
+    uint16_t next_head = Instance->TxHead + 1;
+    if (next_head >= CCX_RX_BUFFER_SIZE)
+    {
+        next_head = 0;
+    }
+
+    if (next_head == Instance->TxTail)
+    {
+        return;
+    }
+
+    Instance->TxHead = next_head;
+
+    Instance->TxBuf[Instance->TxHead].ID = ID;
+    Instance->TxBuf[Instance->TxHead].DLC = DLC;
+    Instance->TxBuf[Instance->TxHead].IDE_flag = IDE_flag;
+
+    if ((DLC > 0) && (DLC <= 8))
+    {
+        CopyBuf(Data, Instance->TxBuf[Instance->TxHead].Data, DLC);
+    }
+}
+
+
+static inline void CCX_TX_MsgFromTables(CCX_instance_t *Instance)
+{
+    assert(NULL != Instance);
+    uint8_t Tmp[8];
+
+    for (uint16_t i = 0; i < Instance->TxTableSize; i++)
+    {
+        if (CCX_GET_TICK - Instance->CCX_TX_table[i].LastTick >= Instance->CCX_TX_table[i].SendFreq)
+        {
+            Instance->CCX_TX_table[i].LastTick = CCX_GET_TICK;
+            CopyBuf(Instance->CCX_TX_table[i].Data, Tmp, Instance->CCX_TX_table[i].DLC);
+            if (NULL != Instance->CCX_TX_table[i].Parser)
+            {
+                Instance->CCX_TX_table[i].Parser(Instance, Tmp, i);
+            }
+            CCX_TX_PushMsg(Instance, Instance->CCX_TX_table[i].ID, Tmp, Instance->CCX_TX_table[i].DLC,
+                          Instance->CCX_TX_table[i].IDE_flag);
+        }
+    }
+}
+
+
+
+static inline void CCX_TX_Poll(CCX_instance_t *Instance)
+{
+    assert((NULL != Instance) && (NULL != Instance->BusCheck));
+
+    CCX_TX_MsgFromTables(Instance);
+
+    while ((Instance->TxHead != Instance->TxTail) && (Instance->BusCheck(Instance) == CCX_BUS_FREE))
+    {
+        Instance->TxTail++;
+        if (Instance->TxTail >= CCX_TX_BUFFER_SIZE)
+        {
+            Instance->TxTail = 0;
+        }
+
+        assert(NULL != Instance->SendFunction);
+        Instance->SendFunction(Instance, &Instance->TxBuf[Instance->TxTail]);
+    }
+}
+
+void CCX_Poll(CCX_instance_t *Instance)
+{
+	CCX_RX_Poll(Instance);
+	CCX_TX_Poll(Instance);
+}
+
+void CCX_Init(CCX_instance_t *Instance, CCX_RX_table_t* CCX_RX_table, CCX_TX_table_t* CCX_TX_table, uint16_t RxTableSize,
+		uint16_t TxTableSize, void (*SendFunction)(const CCX_instance_t *Instance, const CCX_message_t *msg),
+		CCX_BusIsFree_t (*BusCheck)(const CCX_instance_t *Instance), void (*TimeoutCallback)(CCX_instance_t *Instance, uint16_t Slot),
+		void (*ParserUnregMsg)(const CCX_instance_t *Instance, CCX_message_t *Msg))
+{
+	/* asserts here */
+	Instance->CCX_TX_table = CCX_TX_table;
+	Instance->CCX_RX_table = CCX_RX_table;
+	Instance->RxTableSize = RxTableSize;
+	Instance->TxTableSize = TxTableSize;
+	Instance->SendFunction = SendFunction;
+	Instance->BusCheck = BusCheck;
+	Instance->TimeoutCallback = TimeoutCallback;
+	Instance->Parser_unreg_msg = ParserUnregMsg;
+}
+
+
+
+
+
+
---
+++ can_corex.h
@@ -0,0 +1,202 @@
+/*
+ * can_corex.h
+ *
+ *  Created on: Aug 20, 2025
+ *      Author: Adrian
+ */
+
+#ifndef CAN_COREX_CAN_COREX_H_
+#define CAN_COREX_CAN_COREX_H_
+
+
+#include <limits.h>
+#include <stdint.h>
+
+
+/**
+ * @def CCX_TICK_FROM_FUNC
+ * @brief Enables system tick retrieval via a function call.
+ *
+ * If set to 1, the system time base is obtained by calling a function
+ * that returns the current tick count.
+ * If set to 0, the system time base is read directly from a variable.
+ */
+#define CCX_TICK_FROM_FUNC 0
+
+/**
+ * @def CCX_RX_BUFFER_SIZE
+ * @brief Size of the CAN receive buffer.
+ *
+ * Defines the number of messages that can be stored in the CAN RX buffer.
+ */
+#define CCX_RX_BUFFER_SIZE 48
+
+/**
+ * @def CCX_TX_BUFFER_SIZE
+ * @brief Size of the CAN transmit buffer.
+ *
+ * Defines the number of messages that can be stored in the CAN TX buffer.
+ */
+#define CCX_TX_BUFFER_SIZE 48
+
+/**
+ * @brief System time base type and maximum timeout definition.
+ *
+ * By default, the system tick type is `volatile uint32_t`.
+ * If you want to use a different type, define `CC_TIME_BASE_TYPE_CUSTOM` as that type
+ * and also define the corresponding `_IS_` macro
+ * (e.g., `CC_TIME_BASE_TYPE_CUSTOM_IS_UINT16`) to properly set `CC_MAX_TIMEOUT`.
+ */
+#ifndef CCX_TIME_BASE_TYPE_CUSTOM
+
+#define CCX_MAX_TIMEOUT UINT32_MAX
+typedef volatile uint32_t CCX_TIME_t;
+
+#else
+
+typedef CCX_TIME_BASE_TYPE_CUSTOM CC_TIME_t;
+
+#if defined(CCX_TIME_BASE_TYPE_CUSTOM_IS_UINT8)
+#define CCX_MAX_TIMEOUT UINT8_MAX
+#elif defined(CCX_TIME_BASE_TYPE_CUSTOM_IS_UINT16)
+#define CCX_MAX_TIMEOUT UINT16_MAX
+#elif defined(CCX_TIME_BASE_TYPE_CUSTOM_IS_UINT32)
+#define CCX_MAX_TIMEOUT UINT32_MAX
+#elif defined(CCX_TIME_BASE_TYPE_CUSTOM_IS_UINT64)
+#define CCX_MAX_TIMEOUT UINT64_MAX
+#elif defined(CCX_TIME_BASE_TYPE_CUSTOM_IS_INT8)
+#define CCX_MAX_TIMEOUT INT8_MAX
+#elif defined(CCX_TIME_BASE_TYPE_CUSTOM_IS_INT16)
+#define CCX_MAX_TIMEOUT INT16_MAX
+#elif defined(CCX_TIME_BASE_TYPE_CUSTOM_IS_INT32)
+#define CCX_MAX_TIMEOUT INT32_MAX
+#elif defined(CCX_TIME_BASE_TYPE_CUSTOM_IS_INT64)
+#define CCX_MAX_TIMEOUT INT64_MAX
+#else
+#error "CCX_MAX_TIMEOUT: Unknown CCX_TIME_BASE_TYPE_CUSTOM or missing _IS_* define"
+#endif
+
+#endif
+
+
+
+/**
+ * @brief Enumeration indicating the status of the CAN bus.
+ *
+ * This enum represents whether the CAN bus is free for transmitting a new message
+ * or if it is currently busy.
+ *
+ * Values:
+ * - CCX_BUS_BUSY: The CAN bus is currently busy and cannot accept new messages.
+ * - CCX_BUS_FREE: The CAN bus is free and ready for new message transmission.
+ */
+typedef enum
+{
+    CCX_BUS_BUSY = 0,
+    CCX_BUS_FREE
+} CCX_BusIsFree_t;
+
+
+typedef enum
+{
+    CCX_MSG_UNREG,
+    CCX_MSG_REG
+} CCX_MsgRegStatus_t;
+
+/**
+ * @brief Structure representing a CAN message.
+ *
+ * Fields:
+ * - ID: CAN message identifier.
+ * - Data: CAN message data payload (up to 8 bytes).
+ * - DLC: Data Length Code, number of valid data bytes (0-8).
+ * - IDE_flag: Identifier Extension flag (0 = standard, 1 = extended).
+ */
+typedef struct
+{
+    uint32_t ID;
+    uint8_t Data[8];
+    uint8_t DLC : 4;
+    uint8_t IDE_flag : 1;
+} CCX_message_t;
+
+
+
+
+
+typedef struct CCX_instance_t CCX_instance_t;
+
+typedef struct
+{
+    uint32_t ID;
+    uint8_t DLC : 4;
+    uint8_t IDE_flag : 1;
+    CCX_TIME_t TimeOut;
+    void (*Parser)(const CCX_instance_t *Instance, CCX_message_t *Msg, uint16_t Slot);
+    CCX_TIME_t LastTick;
+} CCX_RX_table_t;
+
+typedef struct
+{
+    uint32_t ID;
+    uint8_t *Data;
+    uint8_t DLC : 4;
+    uint8_t IDE_flag : 1;
+    CCX_TIME_t SendFreq;
+    void (*Parser)(const CCX_instance_t *Instance, uint8_t *DataToSend, uint16_t Slot);
+    CCX_TIME_t LastTick;
+} CCX_TX_table_t;
+
+struct CCX_instance_t
+{
+
+	void (*SendFunction)(const CCX_instance_t *Instance, const CCX_message_t *msg);
+	CCX_BusIsFree_t (*BusCheck)(const CCX_instance_t *Instance);
+
+
+	CCX_message_t RxBuf[CCX_RX_BUFFER_SIZE];
+	CCX_message_t TxBuf[CCX_TX_BUFFER_SIZE];
+	uint16_t RxTail, RxHead, TxTail, TxHead;
+	CCX_TIME_t RxReceivedTick[CCX_RX_BUFFER_SIZE];
+
+	CCX_RX_table_t* CCX_RX_table;
+	CCX_TX_table_t* CCX_TX_table;
+	uint16_t RxTableSize, TxTableSize;
+	void (*TimeoutCallback)(CCX_instance_t *Instance, uint16_t Slot);
+	void (*Parser_unreg_msg)(const CCX_instance_t *Instance, CCX_message_t *Msg);
+};
+
+
+void CCX_RX_PushMsg(CCX_instance_t *Instance, uint32_t ID, uint8_t *Data, uint8_t DLC, uint8_t IDE_flag);
+void CCX_TX_PushMsg(CCX_instance_t *Instance, uint32_t ID, uint8_t *Data, uint8_t DLC, uint8_t IDE_flag);
+void CCX_Poll(CCX_instance_t *Instance);
+void CCX_Init(CCX_instance_t *Instance, CCX_RX_table_t* CCX_RX_table, CCX_TX_table_t* CCX_TX_table, uint16_t RxTableSize,
+		uint16_t TxTableSize, void (*SendFunction)(const CCX_instance_t *Instance, const CCX_message_t *msg),
+		CCX_BusIsFree_t (*BusCheck)(const CCX_instance_t *Instance), void (*TimeoutCallback)(CCX_instance_t *Instance, uint16_t Slot),
+		void (*ParserUnregMsg)(const CCX_instance_t *Instance, CCX_message_t *Msg));
+
+
+#if CCX_TICK_FROM_FUNC
+/**
+ * @brief Registers the system tick source function.
+ *
+ * When CCX_TICK_FROM_FUNC is set to 1, this function registers a user-provided
+ * function that returns the current system tick value.
+ *
+ * @param Function Pointer to a function that returns the current system tick (CCX_TIME_t).
+ */
+void CCX_tick_function_register(CCX_TIME_t (*Function)(void));
+#else
+/**
+ * @brief Registers the system tick source variable.
+ *
+ * When CCX_TICK_FROM_FUNC is set to 0, this function registers a pointer
+ * to a variable that holds the current system tick value.
+ *
+ * @param Variable Pointer to a volatile variable of type CCX_TIME_t representing the system tick.
+ */
+void CCX_tick_variable_register(CCX_TIME_t *Variable);
+#endif
+
+
+#endif /* CAN_COREX_CAN_COREX_H_ */
